ec455fef974c6ab018c534c7f938453c26232ae1
scottb <scottb@inyotech.com>
2017-04-20 13:06:25 -0700

    Rework serving ssec downloaded images to better integrate with django.

--- ssec/weather/urls.py
+++ ssec/weather/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 import weather.views
 
@@ -6,3 +8,5 @@ urlpatterns = [
     url(r'^latest_images/(?P<number>\d+)/$', weather.views.latest_images, name='latest_with_count'),
     url(r'^latest_images/$', weather.views.latest_images, name='latest'),
 ]
+
+urlpatterns += static(settings.IMAGES_URL, document_root=settings.IMAGES_PATH)
--- ssec/weather/views.py
+++ ssec/weather/views.py
@@ -10,13 +10,14 @@ def latest_images(request, number=48):
 
     number = int(number)
 
-    path = os.path.join(settings.IMAGES_PATH, 'ssec/image_*.jpg')
+    path = os.path.join(settings.IMAGES_PATH, 'image_*.jpg')
+    url = settings.IMAGES_URL
 
     images = glob.glob(path)
     images.reverse()
     images = [os.path.basename(name) for name in images[:number]]
     images.reverse()
 
-    image_urls = ['"{0}"'.format(static('ssec/'+image)) for image in images]
+    image_urls = ['"{0}"'.format(url + image) for image in images]
 
     return render(request, 'weather/index.html', {'image_urls': image_urls})
